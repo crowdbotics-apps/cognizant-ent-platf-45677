eb0d3246ded6b43102c20596357cb25191fe4ce2
Crowdbotics <team@crowdbotics.com>
2024-01-29 21:29:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cognizant-ent-platf-45677",
+  url: "https://cognizant-ent-platf-45677.botics.co",
+  api: "https://cognizant-ent-platf-45677.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
